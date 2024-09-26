e9caa7dbd35448071811e34251289046ee552032
Ifedolapo <ifedolapo0001@gmail.com>
2024-09-26 04:36:54 +0100

    Home page skeleton

--- src/Healthify_frontend/src/pages/Home.jsx
+++ src/Healthify_frontend/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-white'>
+      <Navbar/>
+
+      <div>
+        <h1>Home</h1>
+        <h2>Home</h2>
+        <h3>Home</h3>
+      </div>
+    </div>
   )
 }
 
