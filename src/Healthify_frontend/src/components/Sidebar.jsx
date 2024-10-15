9b1718baeddd24e4b5b5428c0522f730baba6126
Ifedolapo <ifedolapo0001@gmail.com>
2024-10-15 18:52:09 +0100

    Add donations link

--- src/Healthify_frontend/src/components/Sidebar.jsx
+++ src/Healthify_frontend/src/components/Sidebar.jsx
@@ -4,9 +4,7 @@ const Sidebar = () => {
 	return (
 		<nav className="text-white p-3 flex flex-col gap-4 bg-primary w-[250px] h-full min-h-screen sticky">
 			{/* sticky */}
-			<div className="h-full w-full ">
-
-			</div>
+			<div className="h-full w-full "></div>
 
 			<div className="pb-5">HealthCentre</div>
 			<NavLink
@@ -30,8 +28,8 @@ const Sidebar = () => {
 			>
 				Appointments
 			</NavLink>
-			
-      <NavLink
+
+			<NavLink
 				to={"/user/records"}
 				className={({ isActive }) => {
 					return isActive
@@ -41,8 +39,8 @@ const Sidebar = () => {
 			>
 				Health records
 			</NavLink>
-			
-      <NavLink
+
+			<NavLink
 				to={"/user/telemedicine"}
 				className={({ isActive }) => {
 					return isActive
@@ -50,10 +48,10 @@ const Sidebar = () => {
 						: "p-1 rounded-2xl px-3 w-full";
 				}}
 			>
-        Telemedicine
+				Telemedicine
 			</NavLink>
-			
-      <NavLink
+
+			<NavLink
 				to={"/user/research"}
 				className={({ isActive }) => {
 					return isActive
@@ -61,10 +59,20 @@ const Sidebar = () => {
 						: "p-1 rounded-2xl px-3 w-full";
 				}}
 			>
-        Make money
+				Earn from research
 			</NavLink>
-			
-      <NavLink
+			<NavLink
+				to={"/user/donations"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Donate
+			</NavLink>
+
+			<NavLink
 				to={"/user/settings"}
 				className={({ isActive }) => {
 					return isActive
@@ -72,9 +80,8 @@ const Sidebar = () => {
 						: "p-1 rounded-2xl px-3 w-full";
 				}}
 			>
-        Settings
+				Settings
 			</NavLink>
-
 		</nav>
 	);
 };
