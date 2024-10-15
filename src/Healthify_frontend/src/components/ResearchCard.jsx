a5e0819f3e15d029c7efec15b0c85e649cd0ff7b
Ifedolapo <ifedolapo0001@gmail.com>
2024-10-15 18:58:09 +0100

    Dashboard research card

--- src/Healthify_frontend/src/components/ResearchCard.jsx
+++ src/Healthify_frontend/src/components/ResearchCard.jsx
@@ -1,48 +1,66 @@
+import { Link } from "react-router-dom";
 import { TypographyH6 } from "./Typography";
+import { FaDollarSign } from "react-icons/fa";
+
+const researchList = [
+	{
+		title: "Health Disparity and Public Health",
+		description:
+			"Studying how factors like income, education, and living conditions affect health outcomes, particularly in underserved communities",
+		reward: 150,
+	},
+	{
+		title: "Sleep deprivation",
+		description:
+			"Sleep deprivation affects cognitive function, mood, and overall health, leading to decreased productivity and increased risk of chronic diseases.",
+		reward: 100,
+	},
+	{
+		title: "Effects of Cortisol",
+		description:
+			"Prolonged Cortisol exposure affects cognitive function, mood, and overall health, leading to decreased productivity and increased risk of chronic diseases.",
+		reward: 300,
+	},
+	{
+		title: "Stem Cell Research",
+		description:
+			" Exploring the use of stem cells to regenerate damaged tissues, particularly in heart disease, spinal cord injuries, and diabetes.",
+		reward: 200,
+	},
+];
 
 const ResearchCard = () => {
 	return (
 		<div className="bg-primary p-5 rounded-xl">
-			<TypographyH6 className={"pb-4"}>Earn from research</TypographyH6>
+			<TypographyH6 className={"pb-4 flex flex-row gap-2 items-center"}>
+				Earn from research <FaDollarSign className="text-secondary" />
+			</TypographyH6>
 
 			<div>
 				<div className="flex flex-row gap-3">
-					<div className="rounded-xl p-3 bg-black border border-gray-700">
-						<div className="py-2 flex flex-row justify-between items-center">
-							<h5 className="text-xl font-semibold">
-								{" "}
-								Sleep deprivation
-							</h5>
-							<div>
-								<button className="py-1 px-2 rounded-lg bg-primary text-sm border border-gray-700 font-light">
-									View More
-								</button>
-							</div>{" "}
+					{researchList.slice(0, 2).map((research, index) => (
+						<div
+							key={index}
+							className="rounded-xl p-3 bg-black border border-gray-700"
+						>
+							<div className="py-2 flex flex-row justify-between items-center">
+								<h5 className="text-xl font-semibold">
+									{research.title}
+								</h5>
+								<div>
+									<Link
+										to={"/user/research/" + research.title.split(" ").join("-")}
+										className="py-1 px-2 rounded-lg bg-primary text-sm border border-gray-700 font-light block"
+									>
+										View More
+									</Link>
+								</div>{" "}
+							</div>
+							<div className="text-sm text-gray-400">
+								{research.description}
+							</div>
 						</div>
-						<div className="text-sm text-gray-400">
-							Sleep deprivation affects cognitive function, mood,
-							and overall health, leading to decreased
-							productivity and increased risk of chronic diseases.
-						</div>
-					</div>
-
-					<div className="rounded-xl p-3 bg-black border border-gray-700">
-          <div className="py-2 flex flex-row justify-between items-center">
-							<h5 className="text-xl font-semibold">
-								Effects of Cortisol
-							</h5>
-							<div>
-								<button className="py-1 px-2 rounded-lg bg-primary text-sm border border-gray-700 font-light mr-auto">
-									View More
-								</button>
-							</div>{" "}
-						</div>
-						<div className="text-sm text-gray-400">
-							Prolonged Cortisol exposure affects cognitive function, mood,
-							and overall health, leading to decreased
-							productivity and increased risk of chronic diseases.
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</div>
