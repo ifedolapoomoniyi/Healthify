d3330e17c3c474d83ee4ce803ff51c49d7adc7de
Ifedolapo <ifedolapo0001@gmail.com>
2024-10-15 18:56:07 +0100

    mock data

---
+++ src/Healthify_frontend/src/lib/data.js
@@ -0,0 +1,60 @@
+export const healthData = {
+	gender: "male",
+	bloodgroup: "0+",
+	hospitalID: "HC125728",
+	allergies: "peanuts",
+	dob: "1998/05/12",
+	fullName: "Janeth Doeremi",
+	description: "Hypertension stage 2",
+};
+
+export const upcomingAppointments = [
+	{
+		date: "12th May, 2021",
+		time: "10:00am",
+		priority: "high",
+		title: "MRI Scan",
+	},
+	{
+		date: "15th May, 2021",
+		time: "1:00pm",
+		priority: "medium",
+		title: "Dental Checkup",
+	},
+	{
+		date: "20th May, 2021",
+		time: "3:00pm",
+		priority: "low",
+		title: "Eye Test",
+	},
+];
+
+export const mainDonateCardData = [
+	{
+		img: "https://images.unsplash.com/photo-1612836970304-9a9b6e4c0c3f",
+		title: "Cancer Research",
+		details:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nisl in duvolutpat, nec ultricies mi luctus. Sed nec nunc nec lacus aliquet fermentum.",
+		amountRaised: 2000,
+		balance: 5000,
+		requester: "General Hospital Ikeja",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1612836970304-9a9b6e4c0c3f",
+		title: "Cancer Research",
+		details:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nisl in duvolutpat, nec ultricies mi luctus. Sed nec nunc nec lacus aliquet fermentum.",
+		amountRaised: 2000,
+		balance: 5000,
+		requester: "General Hospital Ikeja",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1612836970304-9a9b6e4c0c3f",
+		title: "Cancer Research",
+		details:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nisl in duvolutpat, nec ultricies mi luctus. Sed nec nunc nec lacus aliquet fermentum.",
+		amountRaised: 2000,
+		balance: 5000,
+		requester: "General Hospital Ikeja",
+	},
+];
