5e319e7d812d1396ee4a2acfabacb3679b7c448b
Ifedolapo <ifedolapo0001@gmail.com>
2024-10-17 23:35:20 +0100

    Homepage skeleton

--- src/Healthify_frontend/src/pages/Home.jsx
+++ src/Healthify_frontend/src/pages/Home.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import hero from "/images/hero.jpg";
+import { TypographyH1 } from "../components/Typography";
 
 const Home = () => {
-  return (
-    <div className='text-white'>
-      <Navbar/>
+	return (
+		<div className="text-white">
+			<Navbar />
 
-      <div>
-        <h1>Home</h1>
-        <h2>Home</h2>
-        <h3>Home</h3>
-      </div>
-    </div>
-  )
-}
+			{/* Hero */}
+			<div className="flex flex-row gap-2 items-center md:p-10 p-7">
+				<div className="flex flex-col flex-1">
+					<TypographyH1 className=" leading-none my-10">
+						Revolutionize Healthcare with Blockchain-Powered Records
+					</TypographyH1>
+					<div className="text-lg italic">
+						Empower patients, healthcare providers, and researchers
+						through secure and transparent blockchain technology.
+					</div>
+				</div>
 
-export default Home
+				<div className="flex-1">
+					<img src={hero} alt="" className="rounded-2xl" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
