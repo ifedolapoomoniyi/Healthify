df18e0e1646c7298007d424d41313e7918987c37
Ifedolapo <ifedolapo0001@gmail.com>
2024-10-20 11:55:36 +0100

    dynamic data in greet component

--- src/Healthify_frontend/src/pages/user/Dashboard.jsx
+++ src/Healthify_frontend/src/pages/user/Dashboard.jsx
@@ -7,8 +7,10 @@ import { Button } from "../../components/ui/Button";
 import UpcomingAppointments from "../../components/UpcomingAppointments";
 import doctor from "/images/doctor.png";
 import { healthData, upcomingAppointments } from "../../lib/data";
+import { useUserStore } from "../../lib/store";
 
 const Dashboard = () => {
+	const { user } = useUserStore();
 	return (
 		<div className="text-white flex flex-row">
 			<Sidebar />
@@ -16,11 +18,11 @@ const Dashboard = () => {
 			<div className="w-full p-5">
 				<TopBar />
 
-				<div className="grid md:grid-cols-2 gap-4">
+				<div className="grid md:grid-cols-2 gap-4 py-5">
 					{/* Greet component */}
 					<div className="p-5 rounded-3xl m-10 bg-secondary space-y-2 relative overflow-hidden">
 						<h1 className="md:text-3xl text-2xl">
-							Good morning, Ifedolapo
+							Good morning, {user?.[0]?.fullName?.split(" ")[0]}
 						</h1>
 						<p>
 							Did you know, an Apple a day could keep the doctor
