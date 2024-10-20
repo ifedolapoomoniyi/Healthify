bd0cf9b2c6d5877b8e8810547454cbc8e0b3bd54
Ifedolapo <ifedolapo0001@gmail.com>
2024-10-20 11:54:37 +0100

    topbar skeleton

---
+++ src/Healthify_frontend/src/components/ProviderSidebar.jsx
@@ -0,0 +1,78 @@
+import { NavLink } from "react-router-dom";
+
+const ProviderSidebar = () => {
+	return (
+		<nav className="text-white p-3 flex flex-col gap-4 bg-primary w-[250px] h-full min-h-screen sticky">
+			{/* sticky */}
+			<div className="h-full w-full "></div>
+
+			<div className="pb-5">HealthCentre</div>
+			<NavLink
+				to={"/provider/dashboard"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Dashboard
+			</NavLink>
+
+			<NavLink
+				to={"/provider/appointments"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Appointments
+			</NavLink>
+
+			<NavLink
+				to={"/provider/records"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Health records
+			</NavLink>
+
+			<NavLink
+				to={"/provider/telemedicine"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Telemedicine
+			</NavLink>
+			<NavLink
+				to={"/provider/donations"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Donate
+			</NavLink>
+
+			<NavLink
+				to={"/provider/settings"}
+				className={({ isActive }) => {
+					return isActive
+						? "bg-secondary p-1 rounded-lg px-3 w-full"
+						: "p-1 rounded-2xl px-3 w-full";
+				}}
+			>
+				Settings
+			</NavLink>
+		</nav>
+	);
+};
+
+export default ProviderSidebar;
